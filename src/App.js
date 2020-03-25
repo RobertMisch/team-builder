cdcbed05005307dc13dbc0e441a1276664b43712
RobertMisch <robertmisch94@gmail.com>
2020-03-25 14:37:30 -0400

    nearly got the stretch

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import './Form'
 import Form from './Form';
 
 const initialTeam = [
-  {name:'fighter', email:'fake1@fake.com', role:'tanky-boi'},
-  {name:'ranger', email:'fake2@fake.com', role:'dps-boi'},
-  {name:'rogue', email:'fake3@fake.com', role:'sneaky-boi'},
+  {id:1, name:'fighter', email:'fake1@fake.com', role:'tanky-boi'},
+  {id:2, name:'ranger', email:'fake2@fake.com', role:'dps-boi'},
+  {id:3, name:'rogue', email:'fake3@fake.com', role:'sneaky-boi'},
 ]
+
+let isEditing=false
 
 function App() {
   const[teamMembers, setTeamMembers] = useState(initialTeam)
   const[formData, setFormData] = useState({
+    id:'',
     name:'',
     email:'',
     role:'',
   })
+  const[member, selectMember] = useState()
+
+
   const onInputChange = event => {
     const inputThatChanged = event.target.name
     const valueOfInput = event.target.value
@@ -23,16 +29,40 @@ function App() {
     setFormData({...formData, [inputThatChanged]:valueOfInput})
 
   }
+
   const onFormSubmit = event => {
     event.preventDefault()
     const newTeamMember = {
+      id:formData.id,
       name:formData.name,
       email:formData.email,
       role:formData.role,
     }
-    setTeamMembers([...teamMembers, newTeamMember])
-
+    if(isEditing){
+      editMember(member)
+    }else{
+      setTeamMembers([...teamMembers, newTeamMember])
+    }
+    
   }
+
+  const memberToEdit = memberObj =>{
+    selectMember(memberObj)
+    setFormData(memberObj)
+    isEditing=true;
+  }
+
+  const editMember = function(member){
+    teamMembers.map((item ,index)=> {
+      if(member.id===item.id){
+        teamMembers[index]=member
+      }
+    })
+    console.log(teamMembers)
+  }
+
+  console.log(member)
+
   return (
     <div className="App">
       <h1>The Party</h1>
@@ -40,10 +70,15 @@ function App() {
         formData={formData}
         onInputChange={onInputChange}
         onFormSubmit={onFormSubmit}
+        memberToEdit={memberToEdit}
+        member={member}
       />
       {
-        teamMembers.map(item => (
-          <div>{item.name} {item.email} {item.role}</div>
+        teamMembers.map((item) => (
+          <div>
+            <div>{item.name} {item.email} {item.role}</div>
+            <button onClick={() => (memberToEdit(item))}>edit</button>
+          </div>
         ))
       }
     </div>
--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Form(props){
+    let count=3
     return(
         <form onSubmit={props.onFormSubmit}>
             <label>name <input
@@ -17,7 +18,7 @@ function Form(props){
             /></label>
             <label>role <input
                 onChange={props.onInputChange} // callback takes an event object
-                value={props.formData.fname}
+                value={props.formData.role}
                 name='role'
                 type='text'
             /></label>
